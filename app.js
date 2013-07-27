dfa03f0879cd9a79caea633f6b00c2cabe4cde32
lasconic <lasconic@gmail.com>
2013-07-28 01:34:25 +0200

    server side logic

--- app.js
+++ app.js
@@ -17,12 +17,36 @@ client = redis.createClient();
 
 app.get('/', routes.index);
 
-app.get('/playlist/push/:id/:secret', function(req, res){
-  var body = req.params.id + " - " + req.params.secret;
-  client.lpush("playlist", req.params.id + ";" + req.params.secret)
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+app.get('/playlist/push', function(req, res){
+  var url = req.url;
+  if(url) {
+    resolveScore(url, 
+      function(resp) {
+        if(resp.statusCode == 200) {
+          var data = '';
+          resp.on('data', function(chunk) {   
+                data += chunk;
+          }).on('error', function(e) {  
+                console.log("Got error: " + e.message); 
+                res.send('Cannot read score metadata')  
+          });
+          resp.on('end', function() {
+            var score = JSON.parse(data); 
+            console.log(score.id + ' - ' + score.secret + ' - ' + score.metadata.pages);                                    
+            var scoreSave = {id:score.id, secret : score.secret, pageCount:score.metadata.pages};
+            client.lpush("playlist", JSON.stringify(scoreSave), function() {
+              //publish new score to connected client
+              //bayeux.getClient().publish('/' + sessionId + '/action', {
+              //  command:"loadscore",
+              // id:score.id
+              //});
+              res.send('Done');
+            }); 
+          });
+        }
+      },
+      function() {res.send('Not a Musescore.com URL');})
+  }
 });
 
 app.get('/playlist/pop', function(req, res){
@@ -31,11 +55,34 @@ app.get('/playlist/pop', function(req, res){
         var body = "";
         if(reply)
           body = reply;
-        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Type', 'application/json');
         res.setHeader('Content-Length', body.length);
         res.end(body);
   });
 });
+
+
+
+var resolveScore = function(msurl, endfunction, errorfunction) {
+ var options = {  
+             host: 'api.musescore.com',   
+             port: 80,   
+             path: '/services/rest/resolve.json?url=' + encodeURIComponent(msurl) + '&oauth_consumer_key=musichackday'  
+        };
+   var req = http.get(options, function(response302) {  
+        if (response302.statusCode == 302) {
+          var scoreURL = urlUtils.parse(response302.headers.location);
+          options = {host: scoreURL.hostname,
+                port:80,
+                path:scoreURL.pathname + scoreURL.search
+                };
+          http.get(options, endfunction); 
+        }  
+   }).on('error', function(e) {  
+        console.log("Got error: " + e.message);
+        errorfunction();   
+   }); 
+}
 
 app.listen(3000);
 console.log('Listening on port 3000');
