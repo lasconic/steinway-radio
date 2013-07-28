b1d686cec84194f3ea8c2a11d02a96ebe72e2941
lasconic <lasconic@gmail.com>
2013-07-28 03:26:21 +0200

    updates

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express')  
 , routes = require('./routes')
 , http = require('http')
-, urlUtils = require('url');
-var app = express();
+, urlUtils = require('url')
+, faye = require('faye')
+, domain = require('domain')
+, serverDomain = domain.create();
+
+var hostname = ""
+var port = 80;
+
+var app = module.exports = express();
+var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 // Configuration
 app.configure(function(){
@@ -16,12 +24,14 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  hostname = 'localhost'; 
+  hostname = 'localhost';
+  port = 3000; 
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
   hostname = 'musescore.no.de';
+  port = 80;
 });
 
 var redis = require("redis"),
@@ -46,13 +56,13 @@ app.get('/playlist/push', function(req, res){
           resp.on('end', function() {
             var score = JSON.parse(data); 
             console.log(score.id + ' - ' + score.secret + ' - ' + score.metadata.pages);                                    
-            var scoreSave = {"id":score.id, "secret" : score.secret, "pageCount":score.metadata.pages};
+            var scoreSave = {"id":score.id, "secret" : score.secret, "pageCount":score.metadata.pages, "permalink":score.permalink, "title":score.title};
             client.lpush("playlist", JSON.stringify(scoreSave), function (error, result) {
               //publish new score to connected client
-              //bayeux.getClient().publish('/' + sessionId + '/action', {
-              //  command:"loadscore",
-              // id:score.id
-              //});
+              bayeux.getClient().publish('/notification', {
+                command:"push",
+                score: JSON.stringify(scoreSave)
+              });
               res.send('Done');
             }); 
           });
@@ -68,13 +78,31 @@ app.get('/playlist/pop', function(req, res){
         var body = "";
         if(reply)
           body = reply;
+        bayeux.getClient().publish('/notification', {
+                command:"pop",
+                score: reply
+              });
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Content-Length', body.length);
         res.end(body);
   });
 });
 
-
+app.get('/playlist', function(req, res){
+  client.lrange("playlist", 0, -1, function (err, replies) {
+        var body = "[";
+        replies.forEach(function (reply, i) {
+            console.log("    " + i + ": " + reply);
+            body += reply;
+            if (i != replies.length-1)
+              body += ",";
+        });
+        body += "]";
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+  });
+});
 
 var resolveScore = function(msurl, endfunction, errorfunction) {
  var options = {  
@@ -97,5 +125,26 @@ var resolveScore = function(msurl, endfunction, errorfunction) {
    }); 
 }
 
-app.listen(3000);
-console.log('Listening on port 3000');
+// Domain for the server
+serverDomain.run(function () {
+
+    var httpServer = http.createServer(function (req, res) {
+
+        var reqd = domain.create();
+        reqd.add(req);
+        reqd.add(res);
+
+        // On error dispose of the domain
+        reqd.on('error', function (error) {
+          console.error('Error', error.code, error.message, req.url);
+          reqd.dispose();
+        })
+
+        // Pass the request to express
+        app(req, res);
+
+    });
+    bayeux.attach(httpServer);
+    httpServer.listen(port);
+    console.log("Express server listening on port %d in %s mode", port, app.get('env'));
+})
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,13 +1,38 @@
  $(document).ready(function() {
- 	$('#submit').click(function() {
- 		var v = $("#msurl").value();
- 		console(v);
- 		if(!empty($.trim(v)) {
- 			$.get("/playlist/push?url=" + $('#msurl').value(), function(data) {
+ 	$('#submit').click(function(event) {
+ 		var v = $("#msurl").val();
+ 		console.log(v);
+ 		if($.trim(v)) {
+ 			$.get("/playlist/push?url=" + v, function(data) {
  					console.log(data);
  					$('#message').html(data);
+ 					$("#msurl").val("");
  			});
  		}
- 		return false;
+ 		event.preventDefault();
  	});
+ 	
+ 	getPlaylist();
+
+	var client = new Faye.Client('/faye');
+    var subscription = client.subscribe('/notification', function(message) {
+    	console.log(message); 
+    	if(message.command === "pop") {
+    		$("#nowplaying").html("");
+    		var score = $.parseJSON(message.score);
+    		$("#nowplaying").append('Now Playing <a href="'+score.permalink+'">'+ score.title + '</a>');
+    	}
+    	getPlaylist();
+    });
+
  });
+
+ function getPlaylist() {
+ 	$.getJSON("/playlist", function(data){
+ 		console.log(data);
+ 		$('#playlist').html('');
+ 		$.each(data.reverse(), function(index, value) {
+		  	$('#playlist').append('<div><a href="'+value.permalink+'">'+ value.title + '</a></div>');
+		});
+ 	});
+ }
